1da35075c82abcb2e4a68be4d9bce07fd85a92bd
john.mappin89@gmail.com <john.mappin89@gmail.com>
2019-04-23 18:53:00 -0400

    Updates

---
+++ precache-manifest.933cf3a72dd3a922a1d578eece03c9d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1126dbf9bec9a6ad6a2",
+    "url": "/BookSearch/static/js/runtime~main.210b1916.js"
+  },
+  {
+    "revision": "b2c09ef81a3d98140bdb",
+    "url": "/BookSearch/static/js/main.4fef1d44.chunk.js"
+  },
+  {
+    "revision": "c1c88bb3aba45fbebd65",
+    "url": "/BookSearch/static/js/2.5bea4f44.chunk.js"
+  },
+  {
+    "revision": "b2c09ef81a3d98140bdb",
+    "url": "/BookSearch/static/css/main.54060d1b.chunk.css"
+  },
+  {
+    "revision": "280a80fc7a41b34d15f0289945672cb7",
+    "url": "/BookSearch/index.html"
+  }
+];
